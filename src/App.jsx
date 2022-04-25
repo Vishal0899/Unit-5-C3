2523715bf332af631b732a3e5dc9c076a925587a
Vishal0899 <97458281+Vishal0899@users.noreply.github.com>
2022-04-25 16:27:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import { Route, Routes } from "react-router-dom";
+import { EmployeeDetails } from "./components/EmployeeDetails";
+import { EmployeeList } from "./components/EmployeeList";
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { Logout } from "./components/Logout";
+import { Navbar } from "./components/Navbar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/employees" element={<EmployeeList/>}></Route>
+        <Route path="/admin" element={<EmployeeDetails/>}></Route>
+        <Route path="/logout" element={<Logout/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/EmployeeDetails" element={<EmployeeDetails/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
