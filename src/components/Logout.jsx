9e52f2250ee5eca038d076d2c1d9cfd1aabb6230
Vishal Pokale <97458281+Vishal0899@users.noreply.github.com>
2022-04-25 17:44:18 +0530

    Delete src directory

--- src/components/Logout.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const Logout = () => {
-  // log user out. it's just an inmemory value in context api
-  return <div></div>;
-};
--- src/components/authCheckComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
-
-export const authCheckComponent = () => {
-  const { isAuth } = useContext(AuthContext);
-
-  return <div>authCheckComponent</div>;
-};
